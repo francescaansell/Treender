063963c0eb88927cf5df94b9f8cff5fcfc2158a7
Francesca <fla5029@psu.edu>
2021-02-07 19:32:07 -0500

    More stuff

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import {
   View,
   Image
 } from 'react-native';
-import { Images, Profiles } from './App/Themes';
+import { Images } from './App/Themes';
+import { Profiles } from './App/Themes';
 import { Feather } from '@expo/vector-icons'; 
 import { Entypo } from '@expo/vector-icons'; 
 
@@ -26,27 +27,29 @@ export default function App() {
       </View>
 
       <View style={styles.middle}>
-        <Image source={Images.like} style={{ width: 200, height: 300 }} /> 
+      
+        <Image source={Profiles.hosler} style={{ maxWidth: 290, height: 300 }} />   
+        <View style={styles.profileText}>
+        <Text style={{fontSize: 24, padding: 5}}>Hosler Oak, #7863</Text>
+        <Text style={{fontsize: 16, padding: 5}}>Arboretum</Text>
+        </View>
       </View>
 
       <View style={styles.bottomAction}>
-        <View>
-          <Image source={Images.rewind} style={{ width: 30, height: 30 }} /> 
+        <View style={styles.smallPic}>
+          <Image source={Images.rewind} style={{width: 21, height: 25 }} /> 
         </View>
-        <View>
+        <View style={styles.bigPic}>
           <Image source={Images.nope} style={{ width: 30, height: 30 }} /> 
         </View>
-        <View>
-          <Image source={Images.boost} style={{ width: 20, height: 40 }} /> 
+        <View style={styles.smallPic}>
+          <Image source={Images.boost} style={{ width: 15, height: 30 }} /> 
         </View>
-        <View>
-          <Image source={Images.nope} style={{ width: 30, height: 30 }} /> 
+        <View style={styles.bigPic}>
+          <Image source={Images.like} style={{ width: 36, height: 30 }} /> 
         </View>
-        <View>
-          <Image source={Images.like} style={{ width: 30, height: 30 }} /> 
-        </View>
-        <View>
-          <Image source={Images.superLike} style={{ width: 30, height: 30 }} /> 
+        <View style={styles.smallPic}>
+          <Image source={Images.superLike} style={{ width: 30, height: 27 }} /> 
         </View>
       </View>
     </View>
@@ -56,35 +59,53 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#F4F4F4',
+    backgroundColor: '#F0F0F0',
     flex: 1,
     flexDirection: 'column',
+    justifyContent: "space-around",
   },
   navbar: {
     flex: 1, 
     flexDirection: 'row',
-    borderWidth: 1,
+    borderBottomWidth: 1,
     borderColor: 'grey',
     justifyContent: 'space-around',
-    height: '56',
+    maxHeight: 56,
+    marginTop: 20,
   },
 
   middle: {
-    marginBottom: 250,
-    marginTop: 200,
     marginLeft: 20,
     justifyContent: 'center',
     align: 'center',
-    borderWidth: 1,
-    bordercolor: 'grey',
-    borderBottomLeftRadius: 5,
-    borderBottomRightRadius: 5,
+    borderWidth: 2,
+    bordercolor: '#a9a9a9',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
     width: 300,
+  },
+
+  profileText: {
+    backgroundColor: 'white',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
   },
   bottomAction: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    borderWidth: 1,
+    justifyContent: 'space-between',
+    padding: 10,
   },
- 
+
+  bigPic: {
+    backgroundColor: 'white',
+    border: 1,
+    borderRadius: 200, 
+    padding: 15,
+  },
+  smallPic: {
+    backgroundColor: 'white',
+    border: 1,
+    borderRadius: 200, 
+   
+  },
 })
